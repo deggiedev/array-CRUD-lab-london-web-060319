c652ccb9c02b2df31482d5a9f0c22e2058cdf6d5
Jack de Grave <jack.degrave@outlook.com>
2019-05-28 20:35:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  james_bond_films ["Goldfinger", "The Spy Who Loved Me", "The Living Daylights", "Skyfall"]
+  james_bond_films = ["Goldfinger", "The Spy Who Loved Me", "The Living Daylights", "Skyfall"]
 end
 
 def add_element_to_end_of_array(array, element)
